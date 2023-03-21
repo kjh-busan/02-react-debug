2a84095834c10ef2a4c402365603e9e0a732c34a
kjh-busan <picsmash7@gmail.com>
2023-03-22 00:29:58 +0900

    fix: comment

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
     { text: 'Do all exercises!', id: 'g1' },
     { text: 'Finish the course!', id: 'g2' }
   ]);
+  // commit
 
   const addGoalHandler = enteredText => {
     setCourseGoals(prevGoals => {
